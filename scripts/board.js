c0ccd0c0036897a203c3c4871ad172acb563fd56
gloedige <gerd.loedige@posteo.de>
2025-11-27 17:05:29 +0100

    create function renderSubtaskProgress and formatSubtaskProgress

--- scripts/board.js
+++ scripts/board.js
@@ -3,4 +3,15 @@ function renderNoTasksToDo(columnId) {
     container.innerHTML = noTasksDoToTemplate(columnId);
 }
 
+function formatSubtaskProgress(subtasks) {
+    const completed = subtasks.filter(subtask => subtask.completed).length;
+    const total = subtasks.length;
+    return `${completed}/${total}`;
+}
+
+function renderSubtaskProgress(elementId, subtasks) {
+    const element = document.getElementById(elementId);
+    element.innerHTML = formatSubtaskProgress(subtasks);
+}
+
 renderNoTasksToDo('toDoContainer');
