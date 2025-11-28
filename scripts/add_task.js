79c844188d43930755b761b374078d3088734391
Lukas Lehmann <lukaslehmann05@gmail.com>
2025-11-28 16:06:00 +0100

    user can now change task priority code and visual wise

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -4,3 +4,77 @@ const task_due_date = document.getElementById("task_due_date")
 const task_assign = document.getElementById("task_assign")
 const task_category = document.getElementById("task_category")
 const task_subtask = document.getElementById("task_subtask")
+
+const low_prio_button = document.getElementById("button_prio_low")
+const medium_prio_button = document.getElementById("button_prio_medium")
+const urgent_prio_button = document.getElementById("button_prio_urgent")
+
+let current_priority = "medium"
+
+function createTask() {
+    console.log("TEST");
+
+}
+
+function checkForRequired() {
+
+}
+
+function clearAllInputs() {
+    task_title.value = ""
+    task_description.value = ""
+    task_subtask.value = ""
+    task_due_date.value = ""
+}
+
+function changePriority(priority) {
+    switch (priority) {
+        case "low":
+            changeToLowPrio()
+            break;
+        case "medium":
+            changeToMedPrio()
+            break
+        case "urgent":
+            changeToUrgentPrio()
+            break
+        default:
+            break;
+    }
+}
+
+function removeOldBackground() {
+    switch (current_priority) {
+        case "low":
+            low_prio_button.classList.remove("bg-green")
+            break;
+        case "medium":
+            medium_prio_button.classList.remove("bg-yellow")
+            medium_prio_button.classList.add("yellow-filter")
+            break;
+        case "urgent":
+            urgent_prio_button.classList.remove("bg-red")
+            break;
+        default:
+            break;
+    }
+}
+
+function changeToLowPrio() {
+    removeOldBackground()
+    current_priority = "low"
+    low_prio_button.classList.add("bg-green")
+}
+
+function changeToMedPrio() {
+    removeOldBackground()
+    current_priority = "medium"
+    medium_prio_button.classList.add("bg-yellow")
+    medium_prio_button.classList.remove("yellow-filter")
+}
+
+function changeToUrgentPrio() {
+    removeOldBackground()
+    current_priority = "urgent"
+    urgent_prio_button.classList.add("bg-red")
+}
