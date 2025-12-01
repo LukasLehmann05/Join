4961f1ecf73882e65d212c81c1c4340c644a9aeb
Fabian Puchta <fabianpuchta@hotmail.de>
2025-12-01 21:00:12 +0100

    refactor: shifted html + css into js template function

--- scripts/contact_template.js
+++ scripts/contact_template.js
@@ -4,5 +4,25 @@ function contactListsingle() {
 }
 
 function contactMain() {
-    return ``
+    return `<div class="main-name">
+                <div class="two-letter-name fullname">AR</div>
+                <div class="name-field">
+                    <p class="fullname">Name Placeholder</p>
+                    <div class="main-name-btns">
+                        <button class="name-btn"><img src="../assets/icons/contacts/edit.svg"
+                            alt="edit contact">Edit</button>
+                        <button class="name-btn"><img src="../assets/icons/contacts/delete.svg"
+                            alt="delete Contact">Delete</button>
+                    </div>
+                </div>
+            </div>
+            <span class="sub-header-information">Contact Information</span>
+            <div class="info-display">
+                <p class="info-text">Email</p>
+                <span>Placeholder Mail</span>
+            </div>
+            <div class="info-display">
+                <p class="info-text">Phone</p>
+                <span>Placeholder Number</span>
+            </div>`
 }
