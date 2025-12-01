07ef83e8fab9567e149128e5cae72ecb2701bfda
Fabian Puchta <fabianpuchta@hotmail.de>
2025-12-01 21:39:01 +0100

    feat: - added display for user in list - added test onclick functions for show purpose - moved html into template functions

--- scripts/contact_template.js
+++ scripts/contact_template.js
@@ -1,5 +1,9 @@
 function contactListsingle() {
-    return `
+    return `<p class="two-letter-name-small">AR</p>
+            <div class="list-info">
+                <p class="font-size-20">Name Placeholder</p>
+                <span class="font-size-16 color-mail">yourmailhere@</span>
+            </div>
     `
 }
 
@@ -9,17 +13,17 @@ function contactMain() {
                 <div class="name-field">
                     <p class="fullname">Name Placeholder</p>
                     <div class="main-name-btns">
-                        <button class="name-btn"><img src="../assets/icons/contacts/edit.svg"
+                        <button class="name-btn font-size-16"><img src="../assets/icons/contacts/edit.svg"
                             alt="edit contact">Edit</button>
-                        <button class="name-btn"><img src="../assets/icons/contacts/delete.svg"
+                        <button class="name-btn font-size-16"><img src="../assets/icons/contacts/delete.svg"
                             alt="delete Contact">Delete</button>
                     </div>
                 </div>
             </div>
-            <span class="sub-header-information">Contact Information</span>
+            <span class="sub-header-information font-size-20">Contact Information</span>
             <div class="info-display">
                 <p class="info-text">Email</p>
-                <span>Placeholder Mail</span>
+                <span class="color-mail">Placeholder Mail</span>
             </div>
             <div class="info-display">
                 <p class="info-text">Phone</p>
---
+++ scripts/contacts.js
@@ -0,0 +1,9 @@
+function displayInMain() {
+    let main = document.getElementById("mainView");
+    main.innerHTML = contactMain();
+}
+
+function displayInList() {
+    let list = document.getElementById("test");
+    list.innerHTML = contactListsingle();
+}
