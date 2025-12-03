61714589807adf1df2a7a3633b9c6e0df2a97ed9
gloedige <gerd.loedige@posteo.de>
2025-12-03 14:45:10 +0100

    move all overlay relating function into board_overlay.js

---
+++ scripts/board_overlay.js
@@ -0,0 +1,148 @@
+function openTaskInOverlay(taskId) {
+    let task = getTaskByTaskId(taskId);
+    document.getElementById('overlay').classList.add('show');
+    setTimeout(() => {
+        document.getElementById('overlay_content').classList.add('show');
+    }, 10);
+    renderOverlayContent(task, taskId);
+}
+
+
+function renderOverlayContent(task, taskId) {
+    const overlayContent = document.getElementById('overlay_content');
+    overlayContent.innerHTML = overlayContentTemplate(task, taskId);
+    renderPriorityIndicator(testTasks.task_id_0123, 'priority_overlay');
+    renderAssignedUserInfos(taskId, 'assigned_users_overlay');
+    renderSubtasksListItems(taskId);
+}
+
+
+function renderAssignedUserInfos(taskId, containerIdSuffix) {
+    let container = document.getElementById(taskId + '_' + containerIdSuffix);
+    let task = getTaskByTaskId(taskId);
+    for (let userId of task.assigned_to) {
+        const user = testUser[userId];
+        const initials = getInitialsFromUser(user);
+        const userName = user.name;
+        let userInfoHtml = assignedUserInfoTemplate(userName, initials);
+        container.innerHTML += userInfoHtml;
+    }
+}
+
+
+function renderSubtasksListItems(taskId) {
+    let containerId = taskId + '_subtasks_list';
+    let container = document.getElementById(containerId);
+    let task = getTaskByTaskId(taskId);
+    let subtaskCounter = 0;
+    for (let subtask of task.subtasks) {
+        subtaskCounter += 1;
+        let subtaskHtml = subtasksListItemTemplate(taskId, subtask.title, subtaskCounter);
+        container.innerHTML += subtaskHtml;
+        renderSubtaskListItemsCheckboxes(taskId, subtaskCounter, subtask.done);
+    }
+}
+
+
+function renderSubtaskListItemsCheckboxes(taskId, subtaskCounter, subtaskDone) {
+    const doneImgPath ="../assets/icons/board/checkbox_done.svg"; 
+    const undoneImgPath ="../assets/icons/board/checkbox_undone.svg";
+    let checkboxCustomId = taskId + '_subtask_checkbox_custom_' + subtaskCounter;
+    let checkboxCustomElement = document.getElementById(checkboxCustomId);
+    if (subtaskDone) {
+        checkboxCustomElement.innerHTML = `<img src="${doneImgPath}" alt="checkbox done icon">`;
+    } else {
+        checkboxCustomElement.innerHTML = `<img src="${undoneImgPath}" alt="checkbox undone icon">`;
+    }
+}
+
+
+function closeOverlay(event) {
+    if(event.target === event.currentTarget) {
+        removeShowClass();
+    }
+}
+
+
+function removeShowClass() {
+    const overlay = document.getElementById('overlay');
+    const contentContent = document.getElementById('overlay_content');
+
+    if (overlay.classList.contains('show')) {
+        contentContent.classList.remove('show');
+         setTimeout(() => {
+            overlay.classList.remove('show');
+        }, 500);
+    } 
+
+}
+
+
+function swapImage(button, isHover) {
+    const img = button.querySelector('img');
+    const normalSrc = button.getAttribute('data-normal-src');
+    const hoverSrc = button.getAttribute('data-hover-src');
+    img.src = isHover ? hoverSrc : normalSrc;
+    let p_tag = button.querySelector('p');
+    if (isHover) {
+        p_tag.style.color = "#29ABE2";
+        p_tag.style.fontFamily = "Inter_Bold";
+    } else {
+        p_tag.style.color = "#2A3647";
+        p_tag.style.fontFamily = "Inter";
+    }
+}
+
+
+function toggleSubtaskDone(taskId, subtaskCounter) {
+    let task = getTaskByTaskId(taskId);
+    let subtaskIndex = subtaskCounter - 1; 
+    task.subtasks[subtaskIndex].done = !task.subtasks[subtaskIndex].done;
+    renderSubtaskListItemsCheckboxes(taskId, subtaskCounter, task.subtasks[subtaskIndex].done);
+    renderSubtaskProgress(task);
+}
+
+
+function openEditTaskOverlay(taskId) {
+    const task = getTaskByTaskId(taskId);
+    const overlayContent = document.getElementById('overlay_content');
+    overlayContent.innerHTML = '';
+    overlayContent.innerHTML = overlayEditTaskTemplate(task, taskId);
+    editTaskTemplateWrapper(task);
+    renderSubtaskEditListItems(taskId);
+}
+
+
+function editTaskTemplateWrapper(task){
+    const taskId = getTaskIdAsStringFromTask(task);
+    const mainContent = document.getElementById('overlay_main_content');
+    let escapeTaskDescription = escapeTextareaContent(task.description);
+    mainContent.innerHTML = `
+    ${overlayEditTaskTitleTemplate(task)}
+    ${overlayEditTaskDescriptionTemplate(escapeTaskDescription)}
+    ${overlayEditTaskDueDateTemplate(task)}
+    ${overlayEditTaskPriorityTemplate(task)}
+    ${overlayEditTaskAssignedUsersTemplate(task)}
+    ${overlayEditTaskSubtasksTemplate(taskId)}`;
+}
+
+
+function renderSubtaskEditListItems(taskId) {
+    let containerId = taskId + '_subtasks_edit_list';
+    let subListContainer = document.getElementById(containerId);
+    let task = getTaskByTaskId(taskId);
+    let subtaskCounter = 0;
+    for (let subtask of task.subtasks) {
+        subtaskCounter += 1;
+        let subtaskHtml = overlayEditSubtaskListItemTemplate(taskId, subtask.title, subtaskCounter);
+        subListContainer.innerHTML += subtaskHtml;
+    }
+}
+
+
+function escapeTextareaContent(text) {
+    return text
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+}
