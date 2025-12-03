135a7b45e7bf99353a49a0693a807ecd21ae1345
Lukas Lehmann <lukaslehmann05@gmail.com>
2025-12-03 10:55:28 +0100

    fixed fetch function

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -4,7 +4,7 @@ function addTaskToDB(task_title, task_description, task_due_date, task_priority,
 }
 
 async function fetchAllData() {
-    let joinFetch = await fetch('https://remotestorage-d19c5-default-rtdb.europe-west1.firebasedatabase.app/join')
+    let joinFetch = await fetch('https://remotestorage-d19c5-default-rtdb.europe-west1.firebasedatabase.app/join.json')
     let joinData = await joinFetch.json()
     return joinData
 }
