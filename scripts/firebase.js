331c1f3ec0bbd9744423b8dd1c87caa42de77641
Lukas Lehmann <lukaslehmann05@gmail.com>
2025-12-04 13:06:21 +0100

    upload new tasks to firebase

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,6 +1,24 @@
 //contacts and subtasks are arrays
-function addTaskToDB(task_title, task_description, task_due_date, task_priority, task_category, all_contacts, all_subtasks) {
+async function addTaskToDB(task_title, task_description, task_due_date, task_priority, task_category, all_contacts, all_subtasks) {
+    const newTask = {
+        "category": task_category,
+        "title": task_title,
+        "description": task_description,
+        "due_date": task_due_date,
+        "priority": task_priority,
+        "assigned_to": all_contacts,
+        "subtasks": all_subtasks,
+    }
 
+    let response = await fetch('https://remotestorage-d19c5-default-rtdb.europe-west1.firebasedatabase.app/join/tasks.json', {
+        method: 'POST',
+        body: JSON.stringify(newTask),
+        header: {
+            'Content-Type': 'application/json'
+        }
+    })
+    let data = await response.json()
+    console.log('Task added:', data)
 }
 
 async function fetchAllData() {
