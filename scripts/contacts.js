c44497d0349aceb5687bf7aa1abb649fb6be6c93
Fabian Puchta <fabianpuchta@hotmail.de>
2025-12-06 16:07:52 +0100

    fix: fixed erros mentioned in review

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,45 +1,29 @@
-function openAddContact() {
-    let dialog =  document.getElementById('addContact');
+function openDialog(id, id2){
+    let dialog =  document.getElementById(id);
     dialog.open = true;
     setTimeout(() => {
-        document.getElementById('addContent').classList.add('show');
-    }, 10);
+        document.getElementById(id2).classList.add('show');
+    }, 10)
+};
+
+function closeDialog(id, id2){
+    let dialog =  document.getElementById(id);
+    document.getElementById(id2).classList.remove('show');
+    setTimeout(() => {
+        dialog.open = false;
+    }, 500)
 };
 
 window.onclick = function (event) {
-    let dialog =  document.getElementById('addContact');
-    if (event.target == dialog) {
-        closeAddContact();
+    let dialog1 =  document.getElementById('addContact');
+    let dialog2 =  document.getElementById('editContact');
+    if (event.target == dialog1) {
+        closeDialog('addContact' , 'addContent');
+    }
+    if (event.target == dialog2) {
+        closeDialog('editContact' , 'editContent');
     }
 };
 
-function closeAddContact() {
-    let dialog =  document.getElementById('addContact');
-    document.getElementById('addContent').classList.remove('show');
-    setTimeout(() => {
-        dialog.open = false;
-    }, 500);
-};
 
-function openEditContact() {
-    let dialog =  document.getElementById('editContact');
-    dialog.open = true;
-    setTimeout(() => {
-        document.getElementById('editContent').classList.add('show');
-    }, 10);
-};
 
-window.onclick = function (event) {
-    let dialog =  document.getElementById('editContact');
-    if (event.target == dialog) {
-        closeEditContact();
-    }
-};
-
-function closeEditContact() {
-    let dialog =  document.getElementById('editContact');
-    document.getElementById('editContent').classList.remove('show');
-    setTimeout(() => {
-        dialog.open = false;
-    }, 500);
-};
