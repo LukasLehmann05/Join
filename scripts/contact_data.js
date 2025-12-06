71ea5889448193823c92b5aa97a5eec55574cd49
Fabian Puchta <fabianpuchta@hotmail.de>
2025-12-06 20:28:07 +0100

    fix: moved contact list section into html template feat: started implementing render functions

--- scripts/contact_data.js
+++ scripts/contact_data.js
@@ -1,10 +1,10 @@
 const colours = [
-     '#aaa827ff',
-     '#dd711fff',
-     '#3567d3ff',
-     '#539e27ff',
+    '#aaa827ff',
+    '#dd711fff',
+    '#3567d3ff',
+    '#539e27ff',
     '#b6322eff',
-     '#992b97ff',
+    '#992b97ff',
     '#8a6710ff',
     '#9077d4ff',
     '#c93360ff'
@@ -14,14 +14,30 @@ async function fetchContactList() {
     let response = await fetch(base_url + ".json");
     let responseJson = await response.json();
     let contactData = responseJson.contacts;
-    renderContactList(contactData);
+
+    for (var contact in contactData) {
+        let name = contactData[contact].name;
+        let email = contactData[contact].email;
+        let letter = name.charAt(0);
+        console.log(letter);
+        
+        await renderListLetter(letter);
+        //renderContactList(contact, email, name);
+        
+    }
+    
+    
 };
 
-function renderContactList(contactData) {
-    let acronym = getAcronym(contactData.contact_id_b.name);
-    let letter = contactData.contact_id_b.name.charAt(0).toLowerCase();
-    document.getElementById(letter).innerHTML += contactListSingle(contactData.contact_id_b, acronym);
-    colorAcronym(contactData.contact_id_a)
+async function renderListLetter(letter) {
+    document.getElementById('contactList').innerHTML += contactListLetterSection(letter);
+};
+
+function renderContactList(contactID, email, name) {
+    let acronym = getAcronym(name);
+    let letter = name.charAt(0).toLowerCase();
+    document.getElementById(letter).innerHTML += contactListSingle(contactID, name, email, acronym);
+    colorAcronym(contactID)
 };
 
 function getAcronym(name) {
@@ -31,9 +47,9 @@ function getAcronym(name) {
 };
 
 function colorAcronym(data) {
-     let element = document.getElementById("listShort" + data);
-     let color = colours[Math.floor(Math.random() * colours.length)];
-     element.style.backgroundColor = color;
+    let element = document.getElementById("listShort" + data);
+    let color = colours[Math.floor(Math.random() * colours.length)];
+    element.style.backgroundColor = color;
 };
 
 function renderContactInMain() {
