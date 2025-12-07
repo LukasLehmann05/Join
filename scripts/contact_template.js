a24e7188f4cacde3c591b93aebacb5228057d31c
Fabian Puchta <fabianpuchta@hotmail.de>
2025-12-07 16:25:36 +0100

    fix: - added missing css class to buttons - added more colors to colour array feat: - added function to display a single user in the main display - integrated data-sets for information

--- scripts/contact_template.js
+++ scripts/contact_template.js
@@ -1,9 +1,9 @@
-function contactListSingle(contactID, name, email, acronym) {
-    return `<div>
-                <p id="listShort${contactID}" class="two-letter-name-small">${acronym}</p>
+function contactListSingle(contactID, name, email, acronym, phone) {
+    return `<div data-id="${contactID}" data-name="${name}" data-email="${email}" data-phone="${phone}" onclick="displayInMain(this)">
+                <p id="short-${contactID}" class="two-letter-name-small">${acronym}</p>
                 <div class="list-info">
-                  <p id="listName${contactID}" class="font-size-20">${name}</p>
-                  <span id="listMail${contactID}" class="font-size-16 color-mail">${email}</span>
+                  <p class="font-size-20">${name}</p>
+                  <span class="font-size-16 color-mail">${email}</span>
                 </div>
             </div>`
 };
@@ -13,20 +13,20 @@ function contactListLetterSection(letter) {
     return `<article class="single-letter-section">
                 <h2 class="single-letter font-size-20">${letter}</h2>
                 <div class="separator-list"></div>
-                <section class="list-user" id="${letter}" onclick="renderContactInMain()"></section>
+                <section class="list-user" id="${letter}"></section>
             </article>`
 };
 
 
-function contactMain() {
+function contactMain(currentId, currentName, currentPhone, currentMail, acronym) {
     return `<div class="main-name">
-                <div id="mainShort" class="two-letter-name fullname">AR</div>
+                <div id="mainShort" class="two-letter-name fullname">${acronym}</div>
                 <div class="name-field">
-                    <p id="mainName" class="fullname">Name Placeholder</p>
+                    <p id="mainName" class="fullname">${currentName}</p>
                     <div class="main-name-btns">
-                        <button class="name-btn font-size-16"><img src="../assets/icons/contacts/edit.svg"
+                        <button onclick="openDialog('editContact' , 'editContent')" class="name-btn font-size-16"><img src="../assets/icons/contacts/edit.svg"
                             alt="edit contact">Edit</button>
-                        <button class="name-btn font-size-16"><img src="../assets/icons/contacts/delete.svg"
+                        <button id="mainShort" class="name-btn font-size-16"><img src="../assets/icons/contacts/delete.svg"
                             alt="delete Contact">Delete</button>
                     </div>
                 </div>
@@ -34,10 +34,10 @@ function contactMain() {
             <span class="sub-header-information font-size-20">Contact Information</span>
             <div class="info-display">
                 <p class="info-text">Email</p>
-                <span id="mainShort" class="color-mail">Placeholder Mail</span>
+                <span id="mainShort" class="color-mail">${currentMail}</span>
             </div>
             <div class="info-display">
                 <p class="info-text">Phone</p>
-                <span id="mainPhone">Placeholder Number</span>
+                <span id="mainPhone">${currentPhone}</span>
             </div>`
 };
