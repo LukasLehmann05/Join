3f68d144e1a8f3164c4339cc6db56346223d3773
gloedige <gerd.loedige@posteo.de>
2025-12-23 15:26:50 +0100

    create dev function getContactColorObj to get color object in board.html

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const contactColorProperty = {};
 const AllData = {};
 
 
+// temporary function to get contact colors on board load
+async function getContactColorObj() {
+    let contactData = (await fetchAllDataGlobal()).contacts;
+    for (let contactID in contactData) {
+        assignColorToContact(contactID);
+    }
+    console.table(contactColorProperty);
+};
 
-
-
+getContactColorObj();
