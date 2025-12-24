de77c710f21006572a6abc64c15f2ac9948f5995
gloedige <gerd.loedige@posteo.de>
2025-12-24 08:10:46 +0100

    update delete function and info for getContactColorObj

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const contactColorProperty = {};
 
 const AllData = {};
 
-
-// temporary function to get contact colors on board load
+// TODO: Refactor to single function for users and contacts
+// Helper function to get contact colors on board load
 async function getContactColorObj() {
     let contactData = (await fetchAllDataGlobal()).contacts;
     for (let contactID in contactData) {
