4eebac8994ec1d5642a710fd0939b710ed8ac608
Fabian Puchta <fabianpuchta@hotmail.de>
2025-12-29 12:39:20 +0100

    fix: various mistakes mentioned by copilot

--- scripts/legal_privacy_help.js
+++ scripts/legal_privacy_help.js
@@ -1,9 +1,11 @@
+/**
+ * function to send user back to referrer site
+ */
 function redirectBack() {
     let referrer = document.referrer;
     if (referrer) {
         window.location.href = referrer;
     } else {
-        
-        window.location.href = '/';
+        window.location.href = '../board.html';
     }
-}
+};
