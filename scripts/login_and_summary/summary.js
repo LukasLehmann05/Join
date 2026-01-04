786cd404066e388124d609ad8c81e0cc3e68d32d
Fabian Puchta <fabianpuchta@hotmail.de>
2026-01-04 14:20:56 +0100

    feat: added js function to change display back to normal - added ids for later user

--- scripts/login_and_summary/summary.js
+++ scripts/login_and_summary/summary.js
@@ -336,7 +336,19 @@ document.addEventListener("DOMContentLoaded", initSummaryPage);
 /**
  * changes backgroundcolor, writing color and appearance of the icon when hovered over a stat in the summary board
  */
-function changeDisplay() {
+function changeDisplay(box, icon, info) {
+  console.log(box, icon, info);
+  
+  
+  
+};
+
+
+/**
+ * reverts changes from function changeDisplay, so the element appears in the original state
+ */
+function changeBackDisplay() {
+  
   
 };
 
