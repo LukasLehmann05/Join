ad9c742ca716ddb3742ca8411c4cbdbd7fea3cc3
Fabian Puchta <fabianpuchta@hotmail.de>
2026-01-10 17:12:46 +0100

    fix: added js.doc comment for script.js via copilot

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const contactColorProperty = {};
 const AllData = {};
 
 // TODO: Refactor to single function for users and contacts
-// Helper function to get contact colors on board load
+/**
+ * Fetches all global data and assigns a color to each contact for board rendering.
+ * Calls `fetchAllDataGlobal()` to retrieve contacts, then invokes
+ * `assignColorToContact(contactID)` for each contact id found.
+ * @returns {Promise<void>}
+ */
 async function getContactColorObj() {
     let contactData = (await fetchAllDataGlobal()).contacts;
     for (let contactID in contactData) {
