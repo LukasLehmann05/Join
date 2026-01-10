94766fa11422afc2035d9815cf53d1b1cb65a782
Fabian Puchta <fabianpuchta@hotmail.de>
2026-01-10 17:36:51 +0100

    fix: removed function

--- script.js
+++ script.js
@@ -25,18 +25,6 @@ const colours = [
 ];
 
 
-// TODO: Refactor to single function for users and contacts
-// Helper function to get contact colors on board load
-async function getContactColorObj() {
-    let contactData = (await fetchAllDataGlobal()).contacts;
-    for (let contactID in contactData) {
-        assignColorToContact(contactID);
-    }
-};
-
-getContactColorObj();
-
-
 /**
  * chooses color for contact randomly from colours array. Gets called in function: "renderContactList()"
  * assigns colors to contact permanently in contactColorProperty
